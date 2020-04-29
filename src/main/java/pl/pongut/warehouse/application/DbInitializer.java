5ce4c6674dffee9c2073a2a53e0d2473534962fa
alek <aleksander.jurczyk@gmail.com>
2020-04-29 16:55:51 +0200

    search: dbInitializer formatting

--- src/main/java/pl/pongut/warehouse/application/DbInitializer.java
+++ src/main/java/pl/pongut/warehouse/application/DbInitializer.java
@@ -25,32 +25,32 @@ public class DbInitializer {
     @PostConstruct
     public void initialize() {
         List<Supplier> toSave = Arrays.asList(
-                createSupplier("111111111111111111111111", "www.supersprzedawca.pl", "+48512022033", "PL", "31-695", "małopolska", "Kraków", "Zamki z piasku"),
-                createSupplier("222222222222222222222222", "www.hugoboss.pl", "+44512031033", "FR", "800-855", "La Defense", "Paryż", "Bonjour"),
-                createSupplier("333333333333333333333333", "www.gawarin.pl", "+41512022033", "UA", "36-458", "ukraine", "Odessa", "Panimaje")
+            createSupplier("111111111111111111111111", "www.supersprzedawca.pl", "+48512022033", "PL", "31-695", "małopolska", "Kraków", "Zamki z piasku"),
+            createSupplier("222222222222222222222222", "www.hugoboss.pl", "+44512031033", "FR", "800-855", "La Defense", "Paryż", "Bonjour"),
+            createSupplier("333333333333333333333333", "www.gawarin.pl", "+41512022033", "UA", "36-458", "ukraine", "Odessa", "Panimaje")
         );
         supplierRepository.saveAll(toSave).subscribe();
 
         productRepository.saveAll(
-                List.of(
-                        Product.builder()._id(new ObjectId("111111111111111111111111")).productName("milk").categoryName("cat1").unitPrice(1d).build(),
-                        Product.builder()._id(new ObjectId("222222222222222222222222")).productName("milkyway").unitPrice(3d).categoryName("cat1").build(),
-                        Product.builder()._id(new ObjectId("333333333333333333333333")).productName("prefixmilkyway").unitPrice(5d).categoryName("cat2").build(),
-                        Product.builder()._id(new ObjectId("444444444444444444444444")).productName("SomeOtherProd").unitPrice(10d).categoryName("cat1").build()
-                )
+            List.of(
+                Product.builder()._id(new ObjectId("111111111111111111111111")).productName("milk").categoryName("cat1").unitPrice(1d).build(),
+                Product.builder()._id(new ObjectId("222222222222222222222222")).productName("milkyway").unitPrice(3d).categoryName("cat1").build(),
+                Product.builder()._id(new ObjectId("333333333333333333333333")).productName("prefixmilkyway").unitPrice(5d).categoryName("cat2").build(),
+                Product.builder()._id(new ObjectId("444444444444444444444444")).productName("SomeOtherProd").unitPrice(10d).categoryName("cat1").build()
+            )
         ).subscribe();
     }
 
     private Supplier createSupplier(String hexId, String homePage, String phone, String country, String postalCode, String region, String city, String companyName) {
         return Supplier.builder()
-                ._id(new ObjectId(hexId))
-                .homePage(homePage)
-                .phone(phone)
-                .country(country)
-                .postalCode(postalCode)
-                .region(region)
-                .city(city)
-                .companyName(companyName)
-                .build();
+            ._id(new ObjectId(hexId))
+            .homePage(homePage)
+            .phone(phone)
+            .country(country)
+            .postalCode(postalCode)
+            .region(region)
+            .city(city)
+            .companyName(companyName)
+            .build();
     }
 }
